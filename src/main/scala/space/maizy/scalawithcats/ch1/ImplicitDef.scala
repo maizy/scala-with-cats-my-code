13488117d591b1aec00b4d99f046f8d9bebf8bbb
Nikita Kovaliov <nikita@maizy.ru>
2018-02-06 09:03:01 +0300

    ch1: implicit def

--- src/main/scala/space/maizy/scalawithcats/ch1/ImplicitDef.scala
+++ src/main/scala/space/maizy/scalawithcats/ch1/ImplicitDef.scala
@@ -1,6 +1,6 @@
 package space.maizy.scalawithcats.ch1
 
-trait Printer[T] {
+trait Printer[-T] {
   def printMe(value: T): String
 }
 
@@ -19,13 +19,36 @@ object StdTypePrinters {
   }
 }
 
+object OptPrinter {
+
+  implicit def optPrinter[T](implicit valuePrinter: Printer[T]): Printer[Option[T]] =
+    new Printer[Option[T]] {
+      override def printMe(value: Option[T]): String = {
+        value match {
+          case Some(v) => "Some[" + valuePrinter.printMe(v) + "]"
+          case None => s"None"
+        }
+      }
+    }
+
+  implicit def nonePrinter: Printer[None.type] =
+    new Printer[None.type] {
+      override def printMe(value: None.type): String = "None!"
+    }
+}
+
 object ImplicitDef {
 
   def main(args: Array[String]): Unit = {
     import StdTypePrinters._
+    import OptPrinter._
 
     println(Printer.printMe(5))
     println(Printer.printMe("test"))
+    println(Printer.printMe(Some("test")))
+    println(Printer.printMe(Option.empty[Int]))
+    println(Printer.printMe(None))
+    println(Printer.printMe(Some(7)))
 
   }
 }
