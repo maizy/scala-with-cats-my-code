4c0283db9c383f0646693c9bd10ef8afdab6c334
Nikita Kovaliov <nikita@maizy.ru>
2018-03-02 08:43:20 +0300

    ch3: codecs: common, imap codec

---
+++ src/test/scala/space/maizy/scalawithcats/ch3/CodecTest.scala
@@ -0,0 +1,31 @@
+package space.maizy.scalawithcats.ch3
+
+/**
+  * Copyright (c) Nikita Kovaliov, maizy.ru, 2018
+  * See LICENSE.txt for details.
+  */
+
+import org.scalatest.{ FlatSpec, Matchers }
+
+class CodecTest extends FlatSpec with Matchers {
+  "Codec" should "works with fully specified codecs" in {
+    import CodecOps._
+    import CodecInstances._
+
+    encode(5) shouldBe "5"
+    encode(true) shouldBe "true"
+    encode("t") shouldBe "t"
+
+    decode[Int]("5") shouldBe 5
+    decode[String]("5") shouldBe "5"
+    decode[Boolean]("yes") shouldBe true
+  }
+
+  it should "works for codec builded from imap" in {
+    import CodecOps._
+    import CodecInstances._
+
+    encode(5.3) shouldBe "5"
+    decode[Double]("5") shouldBe 5.0
+  }
+}
