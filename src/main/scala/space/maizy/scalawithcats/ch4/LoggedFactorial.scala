16f3a49d8b754ba59d7023bd6919f74c29303c31
Nikita Kovaliov <nikita@maizy.ru>
2018-03-23 08:44:04 +0300

    ch4: writer

---
+++ src/main/scala/space/maizy/scalawithcats/ch4/LoggedFactorial.scala
@@ -0,0 +1,36 @@
+package space.maizy.scalawithcats.ch4
+
+/**
+ * Copyright (c) Nikita Kovaliov, maizy.ru, 2018
+ * See LICENSE.txt for details.
+ */
+
+import cats.data.Writer
+import cats.instances.vector._
+import cats.syntax.applicative._
+import cats.syntax.writer._
+
+object LoggedFactorial {
+
+  def slowly[A](body: => A): A = try body finally Thread.sleep(100L)
+
+  def printLnFactorial(n: Int): Int = {
+    val ans = slowly {
+      if (n == 0) 1 else n * printLnFactorial(n - 1)
+    }
+    println(s"${System.currentTimeMillis()}: fact $n $ans")
+    ans
+  }
+
+  type Logged[A] = Writer[Vector[String], A]
+
+  def loggedFactorial(n: Int): Logged[Int] = {
+    for {
+      ans <-
+        slowly {
+          if (n == 0) 1.pure[Logged] else loggedFactorial(n - 1).map(_ * n)
+        }
+      _ <- Vector(s"${System.currentTimeMillis()}: fact $n $ans").tell
+    } yield ans
+  }
+}
---
+++ src/test/scala/space/maizy/scalawithcats/ch4/LoggedFactorialTest.scala
@@ -0,0 +1,43 @@
+package space.maizy.scalawithcats.ch4
+
+/**
+ * Copyright (c) Nikita Kovaliov, maizy.ru, 2018
+ * See LICENSE.txt for details.
+ */
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{ Await, Future }
+import space.maizy.scalawithcats.BaseSpec
+import scala.collection.immutable
+import scala.util.{ Failure, Success }
+
+class LoggedFactorialTest extends BaseSpec {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  "LoggedFactorialTest.printLnFactorial" should "works, but produces messy output" in {
+    val futures = Future.sequence(immutable.Seq(
+      Future(LoggedFactorial.printLnFactorial(5)),
+      Future(LoggedFactorial.printLnFactorial(6))
+    ))
+
+    futures.onComplete(println)
+    Await.ready(futures, Duration.Inf)
+  }
+
+  "LoggedFactorialTest.loggedFactorial" should "works and produce Writable" in {
+    val futures = Future.sequence(immutable.Seq(
+      Future(LoggedFactorial.loggedFactorial(5)),
+      Future(LoggedFactorial.loggedFactorial(6))
+    ))
+
+    futures.onComplete {
+      case Failure(e) => println(e)
+      case Success(r) => r.foreach { resWriter =>
+        val (logs, res) = resWriter.run
+        println(s"Res: $res\nLogs:\n${logs.mkString("\n")}\n")
+      }
+    }
+    Await.ready(futures, Duration.Inf)
+  }
+
+}
