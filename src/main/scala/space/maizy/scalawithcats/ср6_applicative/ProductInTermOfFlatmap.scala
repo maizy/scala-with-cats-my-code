ad285925f99e79415322bc0329a0bd1bf76fcd3e
Nikita Kovaliov <nikita@maizy.ru>
2018-04-11 08:34:26 +0300

    ch6: product in term of flatmap

---
+++ src/main/scala/space/maizy/scalawithcats/ср6_applicative/ProductInTermOfFlatmap.scala
@@ -0,0 +1,21 @@
+package space.maizy.scalawithcats.ср6_applicative
+
+/**
+ * Copyright (c) Nikita Kovaliov, maizy.ru, 2018
+ * See LICENSE.txt for details.
+ */
+
+import cats.Monad
+import cats.syntax.functor._
+import cats.syntax.flatMap._
+
+
+object ProductInTermOfFlatmap {
+
+  def product[M[_]: Monad, A, B](x: M[A], y: M[B]): M[(A, B)] = {
+    for {
+      a <- x
+      b <- y
+    } yield (a, b)
+  }
+}
---
+++ src/test/scala/space/maizy/scalawithcats/ср6_applicative/ProductInTermOfFlatmapTest.scala
@@ -0,0 +1,19 @@
+package space.maizy.scalawithcats.ср6_applicative
+
+/**
+ * Copyright (c) Nikita Kovaliov, maizy.ru, 2018
+ * See LICENSE.txt for details.
+ */
+
+import space.maizy.scalawithcats.BaseSpec
+
+class ProductInTermOfFlatmapTest extends BaseSpec {
+  "ProductInTermOfFlatmap" should "works" in {
+    import cats.Id
+    import cats.syntax.applicative._
+    import cats.instances.list._
+    ProductInTermOfFlatmap.product(5.pure[Id], 6.pure[Id]) shouldBe ((5, 6))
+    ProductInTermOfFlatmap.product(5: Id[Int], 6: Id[Int]) shouldBe ((5, 6))
+    ProductInTermOfFlatmap.product(List(1, 2), List(3, 4)) shouldBe List((1, 3), (1, 4), (2, 3), (2, 4))
+  }
+}
