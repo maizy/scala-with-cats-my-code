25b8b1934e69dab4859801c9f10f302e77a52e1a
Nikita Kovaliov <nikita@maizy.ru>
2018-05-07 19:39:49 +0300

    ch7: traverse for List of Vectors

---
+++ src/test/scala/space/maizy/scalawithcats/ch7_foldable_traverse/TraverseExamplesTest.scala
@@ -0,0 +1,28 @@
+package space.maizy.scalawithcats.ch7_foldable_traverse
+
+/**
+ * Copyright (c) Nikita Kovaliov, maizy.ru, 2018
+ * See LICENSE.txt for details.
+ */
+
+import space.maizy.scalawithcats.BaseSpec
+
+class TraverseExamplesTest extends BaseSpec {
+  "listSequence" should "works for vectors" in {
+    import cats.instances.vector._
+    (TraverseExamples.listSequence(List(Vector(1, 2), Vector(3, 4))): Vector[List[Int]]) shouldBe
+        Vector(
+          List(1, 3),
+          List(1, 4),
+          List(2, 3),
+          List(2, 4)
+        )
+
+    val lists = TraverseExamples.listSequence(List(Vector(1, 2), Vector(3, 4), Vector(5, 6)))
+
+    lists.foreach { l =>
+      l should have size 3
+    }
+    lists should have size Math.pow(2.0, 3.0).toLong
+  }
+}
