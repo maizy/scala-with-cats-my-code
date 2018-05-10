4fe0bbeba2c07af0a7cb6ee2c470729e41176869
Nikita Kovaliov <nikita@maizy.ru>
2018-05-10 09:13:33 +0300

    ch7: traverse with Validated

--- src/main/scala/space/maizy/scalawithcats/ch7_foldable_traverse/TraverseExamples.scala
+++ src/main/scala/space/maizy/scalawithcats/ch7_foldable_traverse/TraverseExamples.scala
@@ -7,11 +7,15 @@ package space.maizy.scalawithcats.ch7_foldable_traverse
 
 import scala.language.higherKinds
 import cats.Applicative
+import cats.data.Validated
 import cats.syntax.applicative._
 import cats.syntax.apply._
+import cats.instances.list._
 import cats.instances.option._
 
 object TraverseExamples {
+
+  type ErrorOr[A] = Validated[List[String], A]
 
   def listTraverse[F[_] : Applicative, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
     list.foldLeft(List.empty[B].pure[F]) { (accum, item) =>
@@ -21,6 +25,15 @@ object TraverseExamples {
   def listSequence[F[_] : Applicative, B](list: List[F[B]]): F[List[B]] =
     listTraverse(list)(identity)
 
-  def process(inputs: List[Int]): Option[List[Int]] =
-    listTraverse(inputs)(n => if(n % 2 == 0) Some(n) else None)
+  def processListOfIntsWithOption(inputs: List[Int]): Option[List[Int]] =
+    listTraverse(inputs)(n => if (n % 2 == 0) Some(n) else None)
+
+  def processListOfIntWithValidated(inputs: List[Int]): ErrorOr[List[Int]] =
+    listTraverse(inputs) { n =>
+      if (n % 2 == 0) {
+        Validated.valid(n)
+      } else {
+        Validated.invalid(List(s"$n is not even"))
+      }
+    }
 }
--- src/test/scala/space/maizy/scalawithcats/ch7_foldable_traverse/TraverseExamplesTest.scala
+++ src/test/scala/space/maizy/scalawithcats/ch7_foldable_traverse/TraverseExamplesTest.scala
@@ -26,12 +26,27 @@ class TraverseExamplesTest extends BaseSpec {
     lists should have size Math.pow(2.0, 3.0).toLong
   }
 
-  "process" should "works" in {
-    TraverseExamples.process(List(2, 4, 6)) shouldBe Some(List(2, 4, 6))
+  "processListOfIntsWithOption" should "works" in {
+    TraverseExamples.processListOfIntsWithOption(List(2, 4, 6)) shouldBe Some(List(2, 4, 6))
 
     // because flatMap used inside semigroupal combine are fail fast
-    TraverseExamples.process(List(1, 2, 3)) shouldBe Option.empty[List[Int]]
-    TraverseExamples.process(List(2, 1, 3)) shouldBe Option.empty[List[Int]]
-    TraverseExamples.process(List(1, 3, 5)) shouldBe Option.empty[List[Int]]
+    TraverseExamples.processListOfIntsWithOption(List(1, 2, 3)) shouldBe Option.empty[List[Int]]
+    TraverseExamples.processListOfIntsWithOption(List(2, 1, 3)) shouldBe Option.empty[List[Int]]
+    TraverseExamples.processListOfIntsWithOption(List(1, 3, 5)) shouldBe Option.empty[List[Int]]
+  }
+
+  "processListOfIntWithValidated" should "works" in {
+    import cats.syntax.validated._
+    TraverseExamples.processListOfIntWithValidated(List(2, 4, 6)) shouldBe List(2, 4, 6).valid[List[String]]
+
+    // because flatMap used inside semigroupal combine are fail fast
+    TraverseExamples.processListOfIntWithValidated(List(1, 2, 3)) shouldBe
+        List("1 is not even", "3 is not even").invalid[List[Int]]
+
+    TraverseExamples.processListOfIntWithValidated(List(2, 1, 3)) shouldBe
+      List("1 is not even", "3 is not even").invalid[List[Int]]
+
+    TraverseExamples.processListOfIntWithValidated(List(1, 3, 5)) shouldBe
+      List("1 is not even", "3 is not even", "5 is not even").invalid[List[Int]]
   }
 }
