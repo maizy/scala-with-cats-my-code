f441c720926ccf20314f2c1ebc0b62bcf746aee6
Nikita Kovaliov <nikita@maizy.ru>
2018-05-29 19:35:40 +0300

    ch9: parallelFoldMap

--- src/main/scala/space/maizy/scalawithcats/ch9_map_reduce/MapReduce.scala
+++ src/main/scala/space/maizy/scalawithcats/ch9_map_reduce/MapReduce.scala
@@ -5,11 +5,29 @@ package space.maizy.scalawithcats.ch9_map_reduce
  * See LICENSE.txt for details.
  */
 
+import scala.concurrent.Future
 import cats.Monoid
+import cats.syntax.monoid._
+import cats.instances.future._
+import cats.instances.vector._
+import cats.syntax.traverse._
 
 object MapReduce {
-  def foldMap[A, B](seq: Vector[A])(f: A => B)(implicit bMonoid: Monoid[B]): B =
-    seq.foldLeft(bMonoid.empty) { (acc, i) =>
-      bMonoid.combine(acc, f(i))
+
+  final val CPUs = Runtime.getRuntime.availableProcessors
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def foldMap[A, B: Monoid](seq: Vector[A])(f: A => B): B =
+    seq.foldLeft(Monoid[B].empty) { (acc, i) =>
+      acc |+| f(i)
     }
+
+  def parallelFoldMap[A, B : Monoid](seq: Vector[A])(f: A => B): Future[B] = {
+    val batches = seq.grouped(CPUs).toVector
+    val computationParts: Future[Vector[B]] = batches
+      .map(batch => Future(foldMap(batch)(f)))
+      .sequence
+
+    computationParts.map(batches => foldMap(batches)(identity))
+  }
 }
--- src/test/scala/space/maizy/scalawithcats/ch9_map_reduce/MapReduceTest.scala
+++ src/test/scala/space/maizy/scalawithcats/ch9_map_reduce/MapReduceTest.scala
@@ -5,6 +5,8 @@ package space.maizy.scalawithcats.ch9_map_reduce
  * See LICENSE.txt for details.
  */
 
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
 import space.maizy.scalawithcats.BaseSpec
 
 class MapReduceTest extends BaseSpec {
@@ -15,6 +17,12 @@ class MapReduceTest extends BaseSpec {
     MapReduce.foldMap(Vector(1, 2, 3))(identity) shouldBe 6
     MapReduce.foldMap(Vector(1, 2, 3))(_.toString + "! ") shouldBe "1! 2! 3! "
     MapReduce.foldMap("Hello world!".toVector)(_.toString.toUpperCase) shouldBe "HELLO WORLD!"
+  }
+
+  "MapReduce.parallelFoldMap" should "works" in {
+    import cats.instances.int._
+    val res = MapReduce.parallelFoldMap((1 to 100).toVector)(identity)
+    Await.result(res, 1.minute) shouldBe 5050
   }
 
 }
