4d4faea3c70bdd7741076eb0223b44d3f5f77bcd
Nikita Kovaliov <nikita@maizy.ru>
2018-06-13 08:32:32 +0300

    ch10: flatmap for Check, fix map

--- src/main/scala/space/maizy/scalawithcats/ch10_case_data_validation/v4_with_predicate/Check.scala
+++ src/main/scala/space/maizy/scalawithcats/ch10_case_data_validation/v4_with_predicate/Check.scala
@@ -5,17 +5,19 @@ package space.maizy.scalawithcats.ch10_case_data_validation.v4_with_predicate
  * See LICENSE.txt for details.
  */
 
-import cats.data.{ NonEmptyList, ValidatedNel }
 import cats.Semigroup
-import PredicateOps._
+import cats.data.{ NonEmptyList, ValidatedNel }
+import space.maizy.scalawithcats.ch10_case_data_validation.v4_with_predicate.PredicateOps._
 
-sealed trait Check[E, A, B] {
-  def apply(a: A)(implicit sg: Semigroup[NonEmptyList[E]]): ValidatedNel[E, B]
+// should be sealed, but used in tests for one hack
+trait Check[E, A, B] {
+  def apply(in: A)(implicit sg: Semigroup[NonEmptyList[E]]): ValidatedNel[E, B]
   def map[C](func: B => C): Check[E, A, C] = CheckMap[E, A, B, C](this, func)
+  def flatMap[C](func: B => Check[E, A, C]): Check[E, A, C] = CheckFlatmap[E, A, B, C](this, func)
 }
 
 object Check {
-  def apply[E, A, B](predicate: Predicate[E, A]) = CheckPure(predicate)
+  def apply[E, A](predicate: Predicate[E, A]): Check[E, A, A] = CheckPure(predicate)
 }
 
 final case class CheckMap[E, A, B, C](check: Check[E, A, B], func: B => C) extends Check[E, A, C] {
@@ -26,4 +28,16 @@ final case class CheckMap[E, A, B, C](check: Check[E, A, B], func: B => C) exten
 final case class CheckPure[E, A](predicate: Predicate[E, A]) extends Check[E, A, A] {
   override def apply(a: A)(implicit sg: Semigroup[NonEmptyList[E]]): ValidatedNel[E, A] =
     predicate.run(a)
+}
+
+final case class CheckFlatmap[E, A, B, C](check: Check[E, A, B], func: B => Check[E, A, C]) extends Check[E, A, C] {
+  override def apply(a: A)(implicit sg: Semigroup[NonEmptyList[E]]): ValidatedNel[E, C] = {
+    val resB: ValidatedNel[E, B] = check(a)
+    resB.withEither { eitherB: Either[NonEmptyList[E], B] =>
+      eitherB.flatMap { bValue =>
+        val checkC: Check[E, A, C] = func(bValue)
+        checkC(a).toEither
+      }
+    }
+  }
 }
--- src/test/scala/space/maizy/scalawithcats/ch10_case_data_validation/v4_with_predicate/CheckTest.scala
+++ src/test/scala/space/maizy/scalawithcats/ch10_case_data_validation/v4_with_predicate/CheckTest.scala
@@ -5,6 +5,8 @@ package space.maizy.scalawithcats.ch10_case_data_validation.v4_with_predicate
  * See LICENSE.txt for details.
  */
 
+import cats.Semigroup
+import cats.data.{ NonEmptyList, ValidatedNel }
 import cats.syntax.validated._
 import space.maizy.scalawithcats.BaseSpec
 
@@ -30,5 +32,27 @@ class CheckTest extends BaseSpec {
     val checkIntAndTransform = intCheck.map(i => s"yay! $i != 0")
     checkIntAndTransform(2) shouldBe "yay! 2 != 0".validNel[ErrorType]
     checkIntAndTransform(0) shouldBe "value is zero".invalidNel[String]
+  }
+
+  it should "supports flatMap" in {
+
+    // there is no other convential way to implement this check
+    val stringCheck = new Check[ErrorType, Int, String] {
+      override def apply(in: Int)(implicit sg: Semigroup[NonEmptyList[ErrorType]]): ValidatedNel[ErrorType, String] = {
+        val strValue = in.toString
+        if (strValue.length > 1) {
+          s"$strValue is right value".validNel[ErrorType]
+        } else {
+          "should be more than 1 char".invalidNel[String]
+        }
+      }
+    }
+
+    val checkIntAndString: Check[ErrorType, Int, String] = intCheck.flatMap { i: Int =>
+      stringCheck
+    }
+    checkIntAndString(20) shouldBe "20 is right value".validNel[ErrorType]
+    checkIntAndString(0) shouldBe "value is zero".invalidNel[String]
+    checkIntAndString(0) shouldBe "value is zero".invalidNel[String]
   }
 }
