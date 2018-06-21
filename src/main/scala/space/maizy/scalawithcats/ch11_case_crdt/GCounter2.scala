0040b6d01d7fdab257802b3105f3d1c03fce1dd6
Nikita Kovaliov <nikita@maizy.ru>
2018-06-21 20:41:08 +0300

    ch11: CRDT case: simplify GCounter2

--- src/main/scala/space/maizy/scalawithcats/ch11_case_crdt/GCounter2.scala
+++ src/main/scala/space/maizy/scalawithcats/ch11_case_crdt/GCounter2.scala
@@ -6,23 +6,19 @@ package space.maizy.scalawithcats.ch11_case_crdt
  */
 
 import cats.Monoid
-import cats.syntax.monoid._
+import cats.syntax.semigroup._
+import cats.instances.map._
 
 final case class GCounter2[A](counters: Map[String, A]) {
 
   def increment(machine: String, amount: A)(implicit monoid: Monoid[A]): GCounter2[A] =
     GCounter2(
-      counters.updated(machine, counters.getOrElse(machine, Monoid[A].empty) |+| amount)
+      counters.updated(machine, counters.getOrElse(machine, monoid.empty) |+| amount)
     )
 
   def merge(that: GCounter2[A])(implicit bsl: BoundedSemiLattice[A]): GCounter2[A] = {
-    val allMachines = (counters.keys ++ that.counters.keys).toList.distinct
-
-    val zero = bsl.empty
     GCounter2(
-      allMachines.map { machine =>
-        machine -> bsl.combine(counters.getOrElse(machine, zero), that.counters.getOrElse(machine, zero))
-      }.toMap
+      this.counters |+| that.counters
     )
   }
 
