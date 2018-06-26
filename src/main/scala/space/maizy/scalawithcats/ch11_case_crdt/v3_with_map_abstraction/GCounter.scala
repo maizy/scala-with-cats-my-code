f2b4e4d6343b33f162195ed98b39caffae14d07b
Nikita Kovaliov <nikita@maizy.ru>
2018-06-26 08:54:24 +0300

    ch11: abstracting over storage

---
+++ src/main/scala/space/maizy/scalawithcats/ch11_case_crdt/v3_with_map_abstraction/GCounter.scala
@@ -0,0 +1,40 @@
+package space.maizy.scalawithcats.ch11_case_crdt.v3_with_map_abstraction
+
+/**
+ * Copyright (c) Nikita Kovaliov, maizy.ru, 2018
+ * See LICENSE.txt for details.
+ */
+
+import scala.language.higherKinds
+import cats.Monoid
+import cats.instances.map._
+import cats.syntax.semigroup._
+import space.maizy.scalawithcats.ch11_case_crdt.BoundedSemiLattice
+
+trait GCounter[F[_, _], K, V] {
+  def increment(storage: F[K, V])(machine: String, amount: V)(implicit monoid: Monoid[V]): F[K, V]
+  def merge(s1: F[K, V], s2: F[K, V])(implicit bsl: BoundedSemiLattice[V]): F[K, V]
+  def total(storage: F[K, V])(implicit monoid: Monoid[V]): V
+}
+
+object GCounter {
+  def apply[F[_, _], K, V](implicit counter: GCounter[F, K, V]): GCounter[F, K, V] = counter
+}
+
+object GCounterMapInstances {
+
+  implicit def mapGCounter[A]: GCounter[Map, String, A] = new GCounter[Map, String, A] {
+
+    override def increment(storage: Map[String, A])(machine: String, amount: A)
+        (implicit monoid: Monoid[A]): Map[String, A] =
+      storage.updated(machine, storage.getOrElse(machine, monoid.empty) |+| amount)
+
+    override def merge(s1: Map[String, A], s2: Map[String, A])
+        (implicit bsl: BoundedSemiLattice[A]): Map[String, A] =
+      s1 |+| s2
+
+    override def total(storage: Map[String, A])(implicit monoid: Monoid[A]): A =
+      monoid.combineAll(storage.values)
+  }
+
+}
